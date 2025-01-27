da0f1c1de3d8a392906d12c2bb4d5bcad71875f4
fmccabewild <fmccabewild@stu.naperville203.org>
2025-01-27 14:13:02 -0600

    created server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const app = express();
+const server = http.createServer(app);
+const dotenv = require("dotenv");
+dotenv.config({path: ".env"});
+const port = process.env.Port || 8080;
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 
+        'http://localhost:3000'
+    );
+    next();
+});
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get()
+server.listen(port, () => {
+    console.log(`The server is listening on port ${port}`);
+});
