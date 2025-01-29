f1cf2345a40f0843a04bed5b9b14a4fe33333f4f
C0dingGM <yutianw2020@gmail.com>
2025-01-29 14:03:15 -0600

    here are the floors maps and hope it works

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+            <li><a href = "basement.html">Basement</a></li>
+            <li><a href = "baseFloor.html">Base Floor</a></li>
+            
+            <li><a href = "bSecondFloor.html">Second Floor</a></li>
+            <li><a href = "bThirdFloor.html">Third Floor</a></li>
+        </ul>
       </header>
     </div>
   );
