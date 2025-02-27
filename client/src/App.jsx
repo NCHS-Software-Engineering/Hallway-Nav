4cc90165ba4264e9d92ee4cbcdb5a2ad6d3b1ae8
Shawn J Plackiyil <SJPlackiyil@stu.naperville203.org>
2025-02-27 14:19:11 -0600

    filling in some rooms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,19 +5,20 @@ import Select from './Components/Select';
 
 function App() {
   const [imageSrc, setImageSrc] = useState(null);
-  const [form, setForm] = useState(-1);
 
   return (
     <div className="App">
       <Barcode />
 
       <ul>
-          <li>{form!==-1 && <><label>Start </label> <Select index={form}/></>}</li>
-          <li>{form!==-1 && <><label>End </label> <Select index={form}/></>}</li>
-          <li><button onClick={() => {setImageSrc(require('./img/basement.png')); setForm(0);}}>Basement</button></li>
-          <li><button onClick={() => {setImageSrc(require('./img/firstFloor.png')); setForm(1);}}>First Floor</button></li>
-          <li><button onClick={() => {setImageSrc(require('./img/secondFloor.png')); setForm(2);}}>Second Floor</button></li>
-          <li><button onClick={() => {setImageSrc(require('./img/thirdFloor.png')); setForm(3);}}>Third Floor</button></li>
+          <li><label>Start </label> <Select /></li>
+          <li><label>End </label> <Select /></li>
+          
+          <li><button onClick={() => setImageSrc(require('./img/basement.png'))}>Basement</button></li>
+          <li><button onClick={() => setImageSrc(require('./img/firstFloor.png'))}>First Floor</button></li>
+          <li><button onClick={() => setImageSrc(require('./img/secondFloor.png'))}>Second Floor</button></li>
+          <li><button onClick={() => setImageSrc(require('./img/thirdFloor.png'))}>Third Floor</button></li>
+          
       </ul>
 
       <figure>
--- client/src/Components/Select.js
+++ client/src/Components/Select.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react";
 
-const Select = (index) => {
+const Select = () => {
     const [options, setOptions] = useState([]);
-    const rooms = [
-        ["000","301","302","303","304","305","306","307","308","309"],
-        ["100","301","302","303","304","305","306","307","308","309"],
-        ["200","301","302","303","304","305","306","307","308","309"],
-        ["300","301","302","303","304","305","306","307","308","309"],
-    ];
-
+    
     useEffect(() => {
+        const rooms = [
+            "B1", "B2", "B3", "B4", "B05A", "B5", "B6",
+            "B7", "B13", "B14", "B15", "B16", "B17",
+            "B18", "B19", "B20", "B20A", "B20B", "B20C",
+            "B21", "B21A", "B22", "B22E", "B23", "B24",
+            "B24B", "B25", "B26", "B29", "B29H", "B29K", "B29M", "B29N", "B50",
+
+
+        ];
+        
+        for(let i = 0; i < 46; i++)
+            rooms.push(200 + i);
+        for(let i = 0; i < 46; i++)
+            rooms.push(300 + i);
+
         let roomSelection = [];
-        for(let i = 0; i < 10; i++) 
-            roomSelection.push(<option>{rooms[3][i]}</option>);
+        for(let i = 0; i < rooms.length; i++) 
+            roomSelection.push(<option>{rooms[i]}</option>);
         setOptions(roomSelection);
     }, []);
 
