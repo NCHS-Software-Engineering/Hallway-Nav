fbbc913375d48070c4150b0b351c29e46c764adf
fmccabewild <fmccabewild@stu.naperville203.org>
2025-04-21 14:14:21 -0500

    json read (don't need to pull)

--- client/src/Cord/PathDisplay.java
+++ client/src/Cord/PathDisplay.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
 import java.util.List;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
 class PathDisplay
 {
     private String finish;
+    private String start = "0";
     public List<String> path;
     private Scanner paths;
-    private Scanner points;
     public static void main(String [] args)
     {
         PathDisplay test = new PathDisplay("0100");
@@ -21,12 +17,11 @@ class PathDisplay
     {
         finish = end;
         String floor = finish.substring(1,1);
-        points = new Scanner(new File("test" + floor + ".csv"));
         paths = new Scanner(new File("test" + floor + ".json"));
     }
 
     public List<String> Find()
     {
-        
+        paths.next("[");
     }
 }
