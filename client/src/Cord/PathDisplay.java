800b78ac66d2beeb8e7a3048e82760a8aff5d5d0
C0dingGM <yutianw2020@gmail.com>
2025-04-22 20:39:22 -0500

    Added new pathing with room 0302

--- client/src/Cord/PathDisplay.java
+++ client/src/Cord/PathDisplay.java
@@ -2,26 +2,24 @@ import java.util.Scanner;
 import java.util.List;
 import java.io.File;
 
-class PathDisplay
-{
+class PathDisplay {
     private String finish;
     private String start = "0";
     public List<String> path;
     private Scanner paths;
-    public static void main(String [] args)
-    {
+
+    public static void main(String[] args) {
         PathDisplay test = new PathDisplay("0100");
         System.out.println(test.Find());
     }
-    public PathDisplay(String end)
-    {
+
+    public PathDisplay(String end) {
         finish = end;
-        String floor = finish.substring(1,1);
+        String floor = finish.substring(1, 1);
         paths = new Scanner(new File("test" + floor + ".json"));
     }
 
-    public List<String> Find()
-    {
+    public List<String> Find() {
         paths.next("[");
     }
 }
