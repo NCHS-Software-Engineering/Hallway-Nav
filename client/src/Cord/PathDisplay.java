d591c972615532b837512c8dc1bc93639f446188
fmccabewild <fmccabewild@stu.naperville203.org>
2025-04-28 14:14:03 -0500

    getting list

--- client/src/Cord/PathDisplay.java
+++ client/src/Cord/PathDisplay.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-import java.util.List;
+import java.util.ArrayList;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 class PathDisplay {
     private String finish;
-    private String start = "0";
-    public List<String> path;
+    public ArrayList<String> path;
     private Scanner paths;
+    private String current;
 
     public static void main(String[] args) {
         PathDisplay test = new PathDisplay("0100");
@@ -15,11 +16,40 @@ class PathDisplay {
 
     public PathDisplay(String end) {
         finish = end;
-        String floor = finish.substring(1, 1);
-        paths = new Scanner(new File("test" + floor + ".json"));
+        String floor = finish.substring(1, 2);
+        try {paths = new Scanner(new File("client/src/Cord/test" + floor + ".json"));}
+        catch(FileNotFoundException e){System.out.println("client/src/Cord/test" + floor + ".json");}
     }
 
-    public List<String> Find() {
-        paths.next("[");
+    public ArrayList<String> Find() {
+        path = new ArrayList<String>();
+        paths.nextLine();
+        while(paths.hasNext())
+        {
+            current = paths.nextLine();
+            current = current.replaceAll("\\s+", "");
+            while(true)
+            {
+                current = paths.nextLine();
+                current = current.replaceAll("\\s+", "");
+                current = current.replaceAll("\"","");
+                current = current.replaceAll(",","");
+                if(current.equals("]"))
+                    break;
+                path.add(current);
+            }
+            if(path.get(path.size()-1).equals(finish))
+            {
+                return path;
+            }
+            else
+            {
+                while(path.size() > 0)
+                {
+                    path.remove(path.size());
+                }
+            }
+        }
+        return (path);
     }
 }
