4521e7aa08240ed7700d6bbbb8c0381497d4ad72
Shawn J Plackiyil <SJPlackiyil@stu.naperville203.org>
2025-04-29 14:21:00 -0500

    starting tts

--- client/src/MapComponentf1.js
+++ client/src/MapComponentf1.js
@@ -2,6 +2,11 @@ import React from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import CustomMap from "./maps/f1.svg"; // Import the SVG file
 import "./App.css";
+function readAltText() {
+  const img = document.querySelector('img');
+  const utterance = new SpeechSynthesisUtterance(img.alt);
+  speechSynthesis.speak(utterance);
+}
 export default function MapComponentf1() {
   return (
     <div className="bg-red-600 min-h-screen flex items-center justify-center">
@@ -10,7 +15,7 @@ export default function MapComponentf1() {
         <img src={CustomMap} alt="Custom SVG Map" className="w-full h-auto"/>
           <svg width="600" height="600" viewBox="0 0 500 500"></svg>
           <img alt="This is the First Floor"></img>
-          <button onclick="readAltText()">Describe image</button>
+          <button onclick={readAltText()}>Describe image</button>
           <script src="tts.js"></script>
         </TransformComponent>
       </TransformWrapper>
