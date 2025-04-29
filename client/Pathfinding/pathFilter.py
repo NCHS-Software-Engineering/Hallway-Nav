aa195cfb43b091d36b5154d0314c968ce5a8c4b8
C0dingGM <yutianw2020@gmail.com>
2025-04-29 00:08:40 -0500

    Finished the corner condition

--- client/Pathfinding/pathFilter.py
+++ client/Pathfinding/pathFilter.py
@@ -25,34 +25,54 @@ class CSVReader:
 
        # print("Loaded Nodes:", self.node_map.keys())
 
-def initiate(node_map):  # Accept the actual node map
+def initiate(node_map):
     try:
         with open('classes.json', 'r') as f:
             all_classes = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
         all_classes = []
 
-    current_classes = []
     try:
-        with open('client\src\Cord\path3.json', 'r') as f:
+        with open('client/src/Cord/path3.json', 'r') as f:
             floor_classes = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
-        floor_classes = []
-        
-    with open("classes.json", "w") as aw:
-        json.dump(all_classes, aw)
+        print("Could not load path3.json")
+        return
 
-        floor = all_classes[2]
+   
 
-        print(f"Floor: {floor}")
-    print(floor_classes)
-    for classes in floor:
-        for paths in floor_classes:
-           if paths[len(paths)-1] ==  classes:
-               print("n")
+    floor = all_classes[2]  # Choose floor
+
+    for class_id in floor:
+        current_paths = []  # Resets for next class id
+        for path in floor_classes:
+            if path and path[-1] == class_id: # Checks if path is empty and access last item
+                current_paths.append(path)
+
+        if current_paths: # Checks if there are any content
+            print(f"\nPaths to destination {class_id}:") # Debug for destination
+            for p in current_paths: #Prints out the paths in a list
+                print(p) #Checks
+            process(current_paths)
+
                
-               
-                
+def process(current_paths):
+    try:
+        with open('classes.json', 'r') as f:
+            all_classes = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        all_classes = []
+
+    
+    if len(current_paths) == 1:
+         all_classes.append(current_paths)
+         with open("thirdFilter.json", "w") as aw:
+            json.dump(all_classes, aw)
+    else:
+        for path in current_paths:
+            #Start calculating distance
+            print()
+
 
 
 
