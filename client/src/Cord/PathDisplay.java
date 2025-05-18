9460180a3136c97ce3233496871f5649955186c2
C0dingGM <yutianw2020@gmail.com>
2025-05-17 22:37:37 -0500

    Now all special rooms can be accessed

--- client/src/Cord/PathDisplay.java
+++ client/src/Cord/PathDisplay.java
@@ -17,36 +17,33 @@ class PathDisplay {
     public PathDisplay(String end) {
         finish = end;
         String floor = finish.substring(1, 2);
-        try {paths = new Scanner(new File("finalFilter.json"));}
-        catch(FileNotFoundException e){System.out.println("Don't Work");}
+        try {
+            paths = new Scanner(new File("finalFilter.json"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Don't Work");
+        }
     }
 
     public ArrayList<String> Find() {
         path = new ArrayList<String>();
         paths.nextLine();
-        while(paths.hasNext())
-        {
+        while (paths.hasNext()) {
             current = paths.nextLine();
             current = current.replaceAll("\\s+", "");
-            while(true)
-            {
+            while (true) {
                 current = paths.nextLine();
                 current = current.replaceAll("\\s+", "");
-                current = current.replaceAll("\"","");
-                current = current.replaceAll(",","");
-                if(current.equals("]"))
+                current = current.replaceAll("\"", "");
+                current = current.replaceAll(",", "");
+                if (current.equals("]"))
                     break;
                 path.add(current);
             }
-            if(path.get(path.size()-1).equals(finish))
-            {
+            if (path.get(path.size() - 1).equals(finish)) {
                 return path;
-            }
-            else
-            {
-                while(path.size() > 0)
-                {
-                    path.remove(path.size()-1);
+            } else {
+                while (path.size() > 0) {
+                    path.remove(path.size() - 1);
                 }
             }
         }
